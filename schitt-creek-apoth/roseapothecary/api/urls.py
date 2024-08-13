a0c77f8001aae2a56b7fc9c3f351fdc90ce01af1
Deneille Guiseppi <deneilleguiseppi@Deneilles-MacBook-Air.local>
2024-08-13 06:56:14 -0400

    updated update_customer_order path in api/urls

--- schitt-creek-apoth/roseapothecary/api/urls.py
+++ schitt-creek-apoth/roseapothecary/api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("customer-orders/", views.CustomerOrdersListCreate.as_view(), name="customer-orders-view-create"),
     path("products/", views.ProductsListCreate.as_view(), name="product-view-create"),
     path("customer-info/<uuid:pk>/", views.CustomerInfoRetrieveUpdateDestroy.as_view(), name="update_customer_info"),
-    path("customer-orders/<int:pk>/", views.CustomerOrdersRetrieveUpdateDestroy.as_view(), name="update_customer_order"),
+    path("customer-orders/<uuid:pk>/", views.CustomerOrdersRetrieveUpdateDestroy.as_view(), name="update_customer_order"),
     path("products/<int:pk>/", views.ProductsRetrieveUpdateDestroy.as_view(), name="update_product")
 ]
